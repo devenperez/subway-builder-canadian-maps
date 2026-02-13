f6f1267d10c0e3bc30c31b1e939256ea84712ad0
Deven Perez <deven.perez@gmail.com>
2026-02-13 17:16:05 -0500

    Fix Windows script

--- mods/canadian-maps/serve.ps1
+++ mods/canadian-maps/serve.ps1
@@ -3,10 +3,10 @@ $wow64 = $env:PROCESSOR_ARCHITEW6432
 
 if ($wow64 -eq "AMD64" -or $arch -eq "AMD64") {
     Write-Host "x86_64 (64-bit)"
-    .\Windows_arm64\pmtiles.exe serve .\map_tiles --port 24575 --cors=*
+    .\pmtiles-exec\Windows_arm64\pmtiles.exe serve .\map_tiles --port 24575 --cors=*
 } elseif ($wow64 -eq "ARM64" -or $arch -eq "ARM64") {
     Write-Host "ARM64"
-    .\Windows_x86_64\pmtiles.exe serve .\map_tiles --port 24575 --cors=*
+    .\pmtiles-exec\Windows_x86_64\pmtiles.exe serve .\map_tiles --port 24575 --cors=*
 } elseif ($arch -eq "x86") {
     Write-Host "x86 (32-bit)"
 } else {
